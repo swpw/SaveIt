091ced943e2a49720fbdc8ad40fc5d6d1b37a5cf
azzlmoa <pawel2070202@wp.pl>
2019-01-17 17:39:30 +0100

    ADD: tab w/ list of words in popup menu

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -22,7 +22,8 @@ function storage(value) {
         "isMarked": false,
         "date": new Date().toString(),
         "added": "popup",
-        "id": new Date().valueOf().toString(),
+        "id": new Date().valueOf().
+          toString(),
         "description": ""
       });
       chrome.storage.sync.set({ "storedText": obj.storedText }, alertAdded(value));
@@ -35,7 +36,8 @@ function storage(value) {
             "isMarked": false,
             "date": new Date().toString(),
             "added": "popup",
-            "id": new Date().valueOf().toString(),
+            "id": new Date().valueOf().
+              toString(),
             "description": ""
           }
         ]
@@ -57,3 +59,51 @@ function alertAdded(word) {
       chrome.notifications.create(options);
   });
 }
+
+//
+
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('nav__tab--open')) {
+    document.querySelector('.nav__tab--open').classList.toggle('nav__tab--open');
+    e.target.classList.toggle('nav__tab--open');
+
+    switch (e.target.textContent) {
+      case 'Add Words':
+        document.querySelector('.form').classList.toggle('form--hidden');
+        document.querySelector('.words').classList.toggle('words--hidden');
+        break;
+
+      case 'Check Words':
+        document.querySelector('.form').classList.toggle('form--hidden');
+        document.querySelector('.words').classList.toggle('words--hidden');
+
+        chrome.storage.sync.get("storedText", obj => {
+          const textList = obj.storedText;
+
+          const removeResults = document.querySelectorAll('.words__li');
+          for (const el of removeResults) {
+            el.remove();
+          }
+
+          textList.forEach(e => {
+            const { word, description } = e;
+
+            const template = `<div class="text">${word}</div>
+                <div class="userText">${description}</div>`;
+
+            const li = document.createElement('li'),
+              ul = document.querySelector('.words__ul');
+
+            li.classList = "words__li";
+            li.innerHTML = template;
+            ul.append(li);
+          });
+        });
+        break;
+    }
+  }
+});
