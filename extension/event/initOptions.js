2f9727eb488679f2a19b78a26ff60c902ae6bfc5
azzlmoa <pawel2070202@wp.pl>
2019-01-17 12:19:17 +0100

    FIX: added default webUrl option parameter on extension install

--- extension/event/initOptions.js
+++ extension/event/initOptions.js
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ "checkbox": true });
 });
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ "webUrl": true });
+});
